843ba292a121bbd35c7be2d9adeb88019083cc10
CarolinaRibeira790 <carolribeirog20@gmail.com>
2025-03-04 14:22:41 -0300

    Criação das telas pedido, todos os pedidos e finalizar pedido

--- src/http/server.ts
+++ src/http/server.ts
@@ -10,6 +10,22 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
+app.get("/todos_pedidos", (req: any, res: any) => {
+    res.render("todos_pedidos");
+})
+
+app.get("/finalizar_pedido", (req: any, res: any) => {
+    res.render("finalizar_pedido");
+})
+
+app.get("/pedido", (req: any, res: any) => {
+    res.render("pedido");
+})
+
+app.get("/perfil", (req: any, res: any) => {
+    res.render("perfil");
+})
+
 app.get("/", (req: any, res: any) => {
     res.render("index");
 });
